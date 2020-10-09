661811eb8ef27046a82da24cc66a8a31ba7f4810
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-09 01:36:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,7 +13,7 @@ class Pokemon
   def save 
     sql = "INSERT INTO pokemon(name, type, db) VALUES(?, ?, ?)"
     DB[:conn].execute(sql, self.name, self.type, self.db)
-    @id = DB[:conn].execute()
+    @id = DB[:conn].execute(SELECT )
   end 
   
   def self.find
