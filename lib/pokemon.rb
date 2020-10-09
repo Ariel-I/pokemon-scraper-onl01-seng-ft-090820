fe4f0ef3b37fb3a1713631db54f8f9afb034bfc7
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-09 01:28:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+  
+  attr_accessor :name, :type, :db
+  attr_reader :id 
+  
+  def initialize(name:, type:, db:, id=nil)
+    @name = name
+    @type = type 
+    @db = db
+    @id = id
+  end
+  
+  def save 
+  end 
+  
+  def self.find
+  end 
+  
 end
