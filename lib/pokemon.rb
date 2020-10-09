5731a01ecc3334c1ef9749dfae389efde259d24c
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-09 02:00:41 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,14 @@ class Pokemon
   end 
   
   def self.find
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end 
   
 end
